3e345b22690c95da3477a0c2d0bd8f5d7ff741b3
Roland Rodriguez <roland@neybor.ly>
2017-01-24 15:42:33 -0800

    added required constants

--- js/api/api.js
+++ js/api/api.js
@@ -5,3 +5,6 @@
 
 
 
+export const FB_GRAPH_URL = "https://graph.facebook.com/v2.8/me?fields=email,name,picture.width(300).height(300)&access_token=";
+export const WETHEPEOPLE_STORAGE_SAVE_KEY = "WETHEPEOPLE_STORAGE_SAVE_KEY";
+export const FB_APP_ID = "ADD YOUR FB APP ID HERE";
