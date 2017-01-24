bb4453d3bc31d69e63af55aaa528e9adda010804
Roland Rodriguez <roland@neybor.ly>
2017-01-24 15:25:38 -0800

    final items

--- js/api/api.js
+++ js/api/api.js
@@ -5,6 +5,3 @@
 
 
 
-export const FB_GRAPH_URL = "https://graph.facebook.com/v2.8/me?fields=email,name,picture.width(300).height(300)&access_token=";
-export const WETHEPEOPLE_STORAGE_SAVE_KEY = "WETHEPEOPLE_STORAGE_SAVE_KEY";
-export const FB_APP_ID = "657094567796730";
